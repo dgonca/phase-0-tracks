761b7f0238662c7551b682fcf75ff1e97f3a0460
dgonca <domstar1595@yahoo.com>
2017-06-12 11:26:30 -0500

    working

---
+++ databases/kitten_maker/85.rb
@@ -0,0 +1,92 @@
+require 'sqlite3'
+
+#what program would make someone's life better? 
+#or my life
+#what do i need
+
+#things to do, this week
+
+
+#create a table for the next week
+#add some things to do
+#be able to run ruby and have user add more things to do as need be
+
+#at the end of the week, delete the table and start over for the next week
+
+todolist = SQLite3::Database.new("todolist.db")
+
+create_list_cmd = <<-SQL
+
+	CREATE TABLE IF NOT EXISTS list_for_the_week(
+
+		id INTEGER PRIMARY KEY,
+		day_of_week VARCHAR(255),
+		level_of_importance INT,
+		item_to_do VARCHAR(255),
+		extra_info VARCHAR(255)
+
+	)
+
+SQL
+
+todolist.execute(create_list_cmd)
+
+def add_item(db)
+	p "What item do you need to add to your list"
+	item = gets.chomp
+	p "What day of the week is this for?"
+	d_o_w = gets.chomp
+	p "What's the level of importance for this item?"
+	importance = gets.chomp
+	p "Is there any extra info you need for this to do list item?, if none skip the entry form."
+	extra = gets.chomp
+
+	db.execute("INSERT INTO list_for_the_week (day_of_week, level_of_importance, item_to_do, extra_info) VALUES (?, ?, ?, ?)", [d_o_w, importance, item, extra])
+
+	# p "Here is your updated list for the week"
+	# db.execute("SELECT * FROM list_for_the_week")
+
+	# db
+
+	p "Here is your updated list"
+	
+	
+end
+
+p "Hello user! We have created a structure for your to do list"
+p "Do you need to create an item?"
+answer = gets.chomp
+
+if answer == "yes" || answer == "Yes"
+	add_item(todolist)
+elsif answer == "no" || answer == "No"
+	p "Here is your to do list"
+	p todolist.execute("SELECT * FROM list_for_the_week")
+else
+	p "Something is wrong with your answer"
+end
+
+# p "Do you need to add an item? Yes or No"
+# answer = gets.chomp
+# case answer
+# 	when answer = "yes"
+# 		answer = "Yes"
+# 	when answer = "no"
+# 		answer = "No"
+# end
+
+# until answer = "No"
+# 	add_item(todolist)
+# 	p "Do you need to add another"
+# end
+
+
+
+
+
+# list_print.each do |item|
+# 	p "#{item['id']} | #{item['day_of_week']} | #{item['item']} | #{item['level_of_importance']} | #{item['extra_info']}"
+# end
+
+
+
--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -12,8 +12,18 @@ db = SQLite3::Database.new("kittens.db")
 
 # learn about fancy string delimiters
 
+create_table_cmd = <<-SQL
+
+	CREATE TABLE IF NOT EXISTS kittens_2(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255), 
+		age INT
+	)
+SQL
+
 
 # create a kittens table (if it's not there already)
+db.execute(create_table_cmd)
 
 
 # add a test kitten
@@ -22,6 +32,16 @@ db = SQLite3::Database.new("kittens.db")
 # add LOOOOTS of kittens!
 # so. many. kittens. 
 #KittenExplosion
+
+db.execute(create_table_cmd)
+def create_kitten(db, name, age)
+  db.execute("INSERT INTO kittens_2 (name, age) VALUES (?, ?)", [name, age])
+end
+
+10000.times do
+  create_kitten(db, Faker::Name.name, 0)
+end
+
 
 
 # explore ORM by retrieving data
@@ -33,22 +53,6 @@ db = SQLite3::Database.new("kittens.db")
 
 
 
-#db = SQLite3::Database.new("kittens.db")
-# db.results_as_hash = true
-# create_table_cmd = <<-SQL
-#   CREATE TABLE IF NOT EXISTS kittens(
-#     id INTEGER PRIMARY KEY,
-#     name VARCHAR(255),
-#     age INT
-#   )
-# SQL
-#db.execute(create_table_cmd)
-# def create_kitten(db, name, age)
-#   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
-# end
 
-# 10000.times do
-#   create_kitten(db, Faker::Name.name, 0)
-# end
 
 
